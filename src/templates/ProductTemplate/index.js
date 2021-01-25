7e9c22f34ec8c77c2f0b98258db271cff235ac69
nison79 <georgenison@gmail.com>
2021-01-25 20:26:24 +0200

    Product Template

---
+++ src/templates/ProductTemplate/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: {eq: $shopifyId}) {
+      title
+    }
+  }
+`;
+
+
+
+
+
+
+export default function ProductTemplate(props){
+  console.log(props);
+  return <h1>Product</h1>;
+}
